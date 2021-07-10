fa8e64588e622d05ea84b63d0a6af1f67341f817
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-07-10 23:08:15 +0600

    bug fixed

--- Extension to Apply ML/js/main.js
+++ Extension to Apply ML/js/main.js
@@ -13,15 +13,117 @@ var alertClass = document.getElementsByClassName("alert");
 var uploadFileDone = false;
 var showResultSection = false;
 var groupImageId = null;
-// const serverUrl="https://adobe-premiere-pro-api-project.herokuapp.com"
-const serverUrl="http://127.0.0.1:8000"
+const serverUrl = "https://adobe-premiere-pro-api-project.herokuapp.com";
+// const serverUrl = "http://127.0.0.1:8000";
+
+var dummyThumbnails = [
+  {
+    id: 56,
+    title: "thumb_0",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_0.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 57,
+    title: "thumb_1",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_1.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 58,
+    title: "thumb_2",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_2.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 59,
+    title: "thumb_3",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_3.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 60,
+    title: "thumb_4",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_4.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 61,
+    title: "thumb_5",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_5.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 62,
+    title: "thumb_6",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_6.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 63,
+    title: "thumb_7",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_7.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 64,
+    title: "thumb_8",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_8.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 65,
+    title: "thumb_9",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_9.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+  {
+    id: 66,
+    title: "thumb_10",
+    thumbnail:
+      "https://adobe-premiere-pro-project-files.s3.us-east-2.amazonaws.com/thumbnails/target_image/thumb_10.jpg",
+    selected: false,
+    groupImage: 8,
+  },
+];
+
 window.onload = function () {
-  
+  // showThumbnails();
+  // var receiveThumbnails = async () => {
+  //   try {
+  //     result = await dummyThumbnails;
+  //     setThumbnails(result);
+  //   } catch (err) {
+  //     showMessage("danger", `ERROR: ${err.message}`);
+  //   }
+  // };
+  // receiveThumbnails();
+
   uploadForm.addEventListener("submit", function (e) {
     e.preventDefault();
     showLoadingScreen();
     var formData = new FormData(uploadForm);
-    var thumbnails = fetch(serverUrl+"/api/uploadGroupImage/", {
+    var thumbnails = fetch(serverUrl + "/api/uploadGroupImage/", {
       method: "POST",
       body: formData,
     })
@@ -46,7 +148,11 @@ window.onload = function () {
     receiveThumbnails();
   });
 };
-
+function showUploadSection() {
+  uploadSection.style.display = "block";
+  pageLoading.style.display = "none";
+  imgListSection.style.display = "none";
+}
 function showThumbnails() {
   uploadSection.style.display = "none";
   pageLoading.style.display = "none";
@@ -98,42 +204,56 @@ imageFirst.onchange = function (evt) {
   }
 };
 
+function delay(sec) {
+  setTimeout(() => {
+    applyMLFunc();
+  }, sec * 1000);
+}
 
 function applyMLFunc() {
-  getSelectedThumbnails()
   var cs = new CSInterface();
-  
-  var myTimeline = fetch(serverUrl+"/api/videoTimeline")
-  .then((response) => response.json())
-  .then((result) => {
-    return result;
-  });
-  
+
+  var myTimeline = fetch(serverUrl + "/api/videoTimeline")
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+
   const generateTimelineInAdobe = async () => {
     result = await myTimeline;
-    if(result.length==0){
-      alert("Video Timelines haven't generated yet. Please keep patience.")
-      return
-    }
-    // console.log(result)
-    videoTimelineJsonData=JSON.parse(result[0].videoTimeline);
-    var jsonString = JSON.stringify(videoTimelineJsonData);
-    var len = jsonString.length;
-    var newJsonString = "";
+    if (result.length == 0) {
+      showMessage(
+        "success",
+        "Video Timelines haven't generated yet. Please keep patience."
+      );
+      delay(20);
+    } else {
+      showMessage("success", ``);
+      showUploadSection();
+      // console.log(result)
+      videoTimelineJsonData = JSON.parse(result[0].videoTimeline);
+      var jsonString = JSON.stringify(videoTimelineJsonData);
+      var len = jsonString.length;
+      var newJsonString = "";
 
-    for (var i = 0; i < len; i++) {
-      if (jsonString[i] == '"') {
-        newJsonString = newJsonString + "\\" + jsonString[i];
-      } else {
-        newJsonString += jsonString[i];
+      for (var i = 0; i < len; i++) {
+        if (jsonString[i] == '"') {
+          newJsonString = newJsonString + "\\" + jsonString[i];
+        } else {
+          newJsonString += jsonString[i];
+        }
       }
+      var sender =
+        'var jsonData="' +
+        newJsonString +
+        '"; var videoFileName="' +
+        result[0].videoFileName +
+        '";';
+      cs.evalScript(sender + "$.runScript.generateTimeline()");
     }
-    var sender = 'var jsonData="' + newJsonString + '"; var videoFileName="'+result[0].videoFileName+'";';
-    cs.evalScript(sender + "$.runScript.generateTimeline()");
   };
   generateTimelineInAdobe();
 }
-
 
 function getSelectedThumbnails() {
   var selectedThumbnails = [];
@@ -144,24 +264,26 @@ function getSelectedThumbnails() {
       thumbnailTitle: selectedClass[i].getAttribute("thumbnailTitle"),
       thumbnailUrl: selectedClass[i].getAttribute("thumbnailUrl"),
     };
-    console.log(singleThumbnail)
+
     selectedThumbnails.push(singleThumbnail);
   }
-  return selectedThumbnails;
+  return JSON.stringify(selectedThumbnails);
 }
 
 function sendSelectedThumbnails() {
-  getSelectedThumbnails()
+  // console.log(getSelectedThumbnails());
   var formData = new FormData();
   formData.append("groupImageId", groupImageId);
   formData.append("selectedThumbnails", getSelectedThumbnails());
-  var sendThumbnails = fetch(serverUrl+"/api/uploadThumbnails/", {
+  var sendThumbnails = fetch(serverUrl + "/api/uploadThumbnails/", {
     method: "POST",
     body: formData,
   })
     .then((response) => response.json())
     .then((result) => {
       showApplyMLScreen();
+      showLoadingScreen();
+      delay(20);
       return result;
     })
     .catch((err) => {
